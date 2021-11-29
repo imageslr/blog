26fb165df5dd547eba3991ff04c36eecc8637cbc
elonz <zhukaihao.r@bytedance.com>
2021-11-29 11:22:19 +0800

    Deploy: config feishu message card
    Change-Id: I613c53c20c973e1595bafb3047fb2d81113bc26a

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,18 +4,29 @@ set -e
 notify() {
   EXIT_CODE=$? # 获取退出状态
   STATUS="Success"
+  COLOR="blue"
   if [ $EXIT_CODE -ne 0 ] 
   then 
     STATUS="Failed" 
+    COLOR="red"
   fi
   echo $STATUS
 
   # 获取提交信息
   GIT_MESSAGE=`git log --format=format:%s -1 ${GIT_COMMIT}`
   CHANGE_ID=`echo $GIT_COMMIT | cut -c 1-10`
-  JSON="{\"msg_type\": \"text\", \"content\": {\"text\": \"Blog Jenkins Build $STATUS\nChangeId: $CHANGE_ID\nMessage: $GIT_MESSAGE\"}}"
+
 
   # 发送飞书通知
+  # 飞书机器人 webhook 指南：
+  #    https://open.feishu.cn/document/ukTMukTMukTM/ucTM5YjL3ETO24yNxkjN
+  # 飞书消息卡片可视化编辑：
+  #    https://open.feishu.cn/tool/cardbuilder?lang=zh-CN
+  # JSON 序列化：
+  #    https://www.bejson.com/zhuanyi/
+  JSON="{\"msg_type\":\"interactive\",\"card\":{\"config\":{\"wide_screen_mode\":true},\"elements\":[{\"tag\":\"div\",\"text\":{\"content\":\"**ChangeID：**$CHANGE_ID\n**Message：**$GIT_MESSAGE\",\"tag\":\"lark_md\"}}],\"header\":{\"template\":\"$COLOR\",\"title\":{\"content\":\"[$STATUS]  Jenkins Build\",\"tag\":\"plain_text\"}}}}"
+  
+
   curl --location --request POST "https://open.feishu.cn/open-apis/bot/v2/hook/$FEISHU_TOKEN" \
   --header 'Content-Type: application/json' \
   --data "$JSON"
